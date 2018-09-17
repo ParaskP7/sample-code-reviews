59fa49b7740b4e6883c1d0895e4e747d3652bcfc
Petros Paraskevopoulos <petros.paraskevopoulos@yahoo.com>
2018-09-17 20:06:31 +0200

    [reviews] init: Add common list classes and resources
    All these common list classes and resources will be reused throughout
    the life of this project. Their main purpose is to display a progress,
    normal or error item on a specific list based on the list's adapter
    status.

---
+++ presentation/src/main/kotlin/io/petros/reviews/presentation/feature/common/list/adapter/AdapterStatus.kt
@@ -0,0 +1,9 @@
+package io.petros.reviews.presentation.feature.common.list.adapter
+
+enum class AdapterStatus {
+
+    IDLE,
+    LOADING,
+    ERROR;
+
+}
---
+++ presentation/src/main/kotlin/io/petros/reviews/presentation/feature/common/list/item/ErrorItemView.kt
@@ -0,0 +1,25 @@
+package io.petros.reviews.presentation.feature.common.list.item
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import io.petros.reviews.R
+import io.petros.reviews.presentation.inflate
+
+class ErrorItemView : FrameLayout {
+
+    constructor(ctx: Context) : super(ctx)
+
+    constructor(ctx: Context, attrs: AttributeSet) : super(ctx, attrs)
+
+    init {
+        inflate(R.layout.item_error)
+        initView()
+    }
+
+    private fun initView() {
+        layoutParams = LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+    }
+
+}
