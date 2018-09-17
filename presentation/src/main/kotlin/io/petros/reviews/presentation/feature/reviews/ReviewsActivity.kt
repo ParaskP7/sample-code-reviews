e9b4be4d42b1a4c64876dae97c9cafa27461a4e2
Petros Paraskevopoulos <petros.paraskevopoulos@yahoo.com>
2018-09-17 14:15:00 +0200

    [reviews] init: Add base activity
    This change moves the main activity to a feature package and makes usage
    of the base activity so that it can utilise all that this base activity
    provides (lifecycle logging, the contact, etc).

---
+++ presentation/src/main/kotlin/io/petros/reviews/presentation/feature/reviews/ReviewsActivity.kt
@@ -0,0 +1,12 @@
+package io.petros.reviews.presentation.feature.reviews
+
+import io.petros.reviews.R
+import io.petros.reviews.presentation.feature.BaseActivity
+
+class ReviewsActivity : BaseActivity() {
+
+    /* CONTRACT */
+
+    override fun constructContentView() = R.layout.activity_reviews
+
+}
